1f5d07992d95265a22571acb1438f47def7d33fb
Easyman121 <2854470@gmail.com>
2024-09-07 23:56:46 +0300

    new

--- Daily/Program.cs
+++ Daily/Program.cs
@@ -2,146 +2,200 @@
 {
     public static void Main()
     {
-        
     }
-    
+
+
     public int GetLucky(string s, int k)
     {
-        string strnew = "";
-        foreach(char c in s)
+        var strnew = "";
+        foreach (var c in s)
         {
-            strnew += (c - 96);
-
+            strnew += c - 96;
         }
-        int result = 0;
-        for (int i =0; i < k; i++)
+
+        var result = 0;
+        for (var i = 0; i < k; i++)
         {
             result = 0;
-            foreach (char c in strnew)
+            foreach (var c in strnew)
             {
-                result += (c - '0');
+                result += c - '0';
             }
+
             strnew = "";
             strnew += result;
         }
+
         return result;
     }
 
-    public  int RobotSim(int[] commands, int[][] obstacles)
+    public int RobotSim(int[] commands, int[][] obstacles)
     {
-        int[] position = new int[] { 0, 0 };
+        var position = new int[] { 0, 0 };
         byte facing = 0;
-        HashSet<(int, int)> obstacleSet = new HashSet<(int, int)>();
-        
-        int furthestPoint = 0;
+        var obstacleSet = new HashSet<(int, int)>();
+
+        var furthestPoint = 0;
         foreach (var obstacle in obstacles)
         {
             obstacleSet.Add((obstacle[0], obstacle[1]));
         }
-        for (int i = 0; i < commands.Length; i++)
-        {
 
+        for (var i = 0; i < commands.Length; i++)
+        {
             switch (commands[i])
             {
-                case -2:
-                    facing = (byte)((facing + 1) % 4);
-                    break;
-                case -1:
-                    facing = (byte) ((facing + 3) % 4);
-                    break;
-                case int j when j > 0 && j < 10:
+            case -2:
+                facing = (byte)((facing + 1) % 4);
+                break;
+            case -1:
+                facing = (byte)((facing + 3) % 4);
+                break;
+            case int j when j > 0 && j < 10:
 
-                    for (j = 0; j < commands[i]; j++)
+                for (j = 0; j < commands[i]; j++)
+                {
+                    var nextX = position[0];
+                    var nextY = position[1];
+                    switch (facing)
                     {
-                        int nextX = position[0];
-                        int nextY = position[1];
-                        switch (facing)
-                        {
-                            case 0:
-                                nextY++;
-                                break;
-                            case 1:
-                                nextX--;
-                                break;
-                            case 3:
-                                nextX++;
-                                break;
-                            case 2:
-                                nextY--;
-                                break;
-                        }
-                        if (obstacleSet.Contains((nextX, nextY)))
-                        {
-                            break;
-                        }
-                        position[0] = nextX;
-                        position[1] = nextY;
-
+                    case 0:
+                        nextY++;
+                        break;
+                    case 1:
+                        nextX--;
+                        break;
+                    case 3:
+                        nextX++;
+                        break;
+                    case 2:
+                        nextY--;
+                        break;
                     }
-                    break;
+
+                    if (obstacleSet.Contains((nextX, nextY)))
+                    {
+                        break;
+                    }
+
+                    position[0] = nextX;
+                    position[1] = nextY;
+                }
+
+                break;
             }
-            int newPoint = position[0] * position[0] + position[1] * position[1];
+
+            var newPoint = position[0] * position[0] + position[1] * position[1];
             if (newPoint > furthestPoint)
             {
                 furthestPoint = newPoint;
             }
         }
+
         return furthestPoint;
     }
 
     public int[] MissingRolls(int[] rolls, int mean, int n)
     {
-        int sum = 0;
-        foreach ( int roll in rolls ) 
+        var sum = 0;
+        foreach (var roll in rolls)
         {
             sum += roll;
         }
-        int remainingSum = mean * (n + rolls.Length) - sum;
-        if (remainingSum > 6 * n || remainingSum < n) return new int[] { };
-        int distributeMean = remainingSum/ n;
-        int mod = remainingSum % n;
-        int[] elements = new int[n];
+
+        var remainingSum = mean * (n + rolls.Length) - sum;
+        if (remainingSum > 6 * n || remainingSum < n)
+        {
+            return new int[] { };
+        }
+
+        var distributeMean = remainingSum / n;
+        var mod = remainingSum % n;
+        var elements = new int[n];
         Array.Fill(elements, distributeMean);
-        for (int i = 0; i < mod; i++)
+        for (var i = 0; i < mod; i++)
         {
             elements[i]++;
         }
+
         return elements;
     }
 
     public class ListNode
     {
-      public int val;
-     public ListNode next;
-      public ListNode(int val = 0, ListNode next = null)
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
         {
             this.val = val;
             this.next = next;
         }
     }
-    public static ListNode ModifiedList(int[] nums, ListNode head)
+
+    public class TreeNode
     {
-        HashSet<int> numSet = new HashSet<int>(nums);
-        ListNode dummy = new ListNode(0, head); 
-        ListNode current = dummy;
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        var numSet = new HashSet<int>(nums);
+        var dummy = new ListNode(0, head);
+        var current = dummy;
 
         while (current.next != null)
         {
             if (numSet.Contains(current.next.val))
             {
-                
                 current.next = current.next.next;
             }
             else
             {
-               
                 current = current.next;
             }
         }
 
         return dummy.next;
-
-
     }
 
+    public bool IsSubPath(ListNode head, TreeNode root)
+    {
+        bool DFS(ListNode head, TreeNode root)
+        {
+            if (head == null)
+            {
+                return true;
+            }
+
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.val != head.val)
+            {
+                return false;
+            }
+
+            return DFS(head.next, root.left) ||
+                   DFS(head.next, root.right);
+        }
+
+        if (root == null)
+        {
+            return false;
+        }
+
+        return DFS(head, root) || IsSubPath(head, root.left) || IsSubPath(head, root.right);
+    }
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,23 +1,24 @@
-﻿
-public class Solution
+﻿public class Solution
 {
     public static void Main()
     {
-        
+        var a = "  hello world  ";
+        Console.WriteLine($"1{ReverseWords(a)}1");
         Console.ReadLine();
     }
+
     public string MergeAlternately(string word1, string word2)
     {
-        string result = "";
-        int i = 0;
-        int j = 0;
+        var result = "";
+        var i = 0;
+        var j = 0;
         while (i < word1.Length && j < word2.Length)
         {
             result += word1[i].ToString() + word2[j].ToString();
             i++;
             j++;
         }
-        
+
         if (j < word2.Length)
         {
             result += word2.Substring(j, word2.Length - j);
@@ -25,25 +26,25 @@ public class Solution
         else if (i < word1.Length)
         {
             result += word1.Substring(i, word1.Length - i);
-            
         }
+
         return result;
     }
 
     public string GcdOfStrings(string str1, string str2)
     {
-        string prefix = "";
-        string result ="";
-        int str1l = str1.Length;
-        int str2l = str2.Length;
-        for (int i = 0; i < str1l && i < str2l; i++)
+        var prefix = "";
+        var result = "";
+        var str1l = str1.Length;
+        var str2l = str2.Length;
+        for (var i = 0; i < str1l && i < str2l; i++)
         {
             prefix += str1[i].ToString();
-              
-            int prefl = prefix.Length;
+
+            var prefl = prefix.Length;
             if (str1l % prefl == 0 && str2l % prefl == 0)
             {
-                if (str1 == string.Concat(Enumerable.Repeat(prefix, str1l/ prefl))
+                if (str1 == string.Concat(Enumerable.Repeat(prefix, str1l / prefl))
                     && str2 == string.Concat(Enumerable.Repeat(prefix, str2l / prefl)))
                 {
                     result = prefix;
@@ -52,16 +53,14 @@ public class Solution
         }
 
         return result;
-        
     }
 
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
     {
-        
-        int len = candies.Length;
+        var len = candies.Length;
         IList<bool> result = new List<bool>(len);
-        int maxCandies = candies.Max();
-        for (int i = 0; i < len; i++)
+        var maxCandies = candies.Max();
+        for (var i = 0; i < len; i++)
         {
             if (candies[i] + extraCandies >= maxCandies)
             {
@@ -96,7 +95,7 @@ return result;
 
     public bool CanPlaceFlowers(int[] flowerbed, int n)
     {
-        for (int i = 0; i < flowerbed.Length; i++)
+        for (var i = 0; i < flowerbed.Length; i++)
         {
             if (flowerbed[i] == 0 && n != 0)
             {
@@ -123,21 +122,117 @@ return result;
                     flowerbed[i] = 1;
                     n--;
                 }
-                
             }
-            
         }
+
         return n == 0 ? true : false;
     }
 
     public int ScoreOfString(string s)
     {
-        int result = 0;
-        for (int i = 0; i < s.Length-1; i++)
+        var result = 0;
+        for (var i = 0; i < s.Length - 1; i++)
         {
-            int temp = (int)s[i] - (int)s[i + 1];
+            var temp = (int)s[i] - (int)s[i + 1];
             result += (temp + (temp >> 31)) ^ (temp >> 31);
         }
+
         return result;
+    }
+
+    public string ReverseVowels(string s)
+    {
+        int strt = 0, end = s.Length - 1;
+        var a = new HashSet<char>() { 'a', 'A', 'e', 'E', 'o', 'O', 'u', 'U', 'i', 'I' };
+        var arr = s.ToCharArray();
+        bool b = false, c = false;
+        while (strt < end)
+        {
+            if (!a.Contains(s[strt]))
+            {
+                strt++;
+            }
+            else
+            {
+                b = true;
+            }
+
+            if (strt < end)
+            {
+                if (a.Contains(s[end]))
+                {
+                    c = true;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+
+            if (b && c)
+            {
+                (arr[strt], arr[end]) = (arr[end], arr[strt]);
+                strt++;
+                end--;
+                b = c = false;
+            }
+        }
+
+        return new string(arr);
+    }
+
+    public static string NormalizeWhiteSpace(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return string.Empty;
+        }
+
+        var current = 0;
+        var output = new char[input.Length];
+        var skipped = false;
+
+        foreach (var c in input.ToCharArray())
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                if (!skipped)
+                {
+                    if (current > 0)
+                    {
+                        output[current++] = ' ';
+                    }
+
+                    skipped = true;
+                }
+            }
+            else
+            {
+                skipped = false;
+                output[current++] = c;
+            }
+        }
+
+        return new string(output, 0, current);
+    }
+
+    public static string ReverseWords(string s)
+    {
+        string[] arr = s.Split(' ');
+        s = "";
+        for (var i = arr.Length - 1; i > -1; i--)
+        {
+            if (arr[i] != "")
+            {
+                s += arr[i] + " ";
+            }
+        }
+
+        if (s[s.Length - 1] == ' ')
+        {
+            s = s.Remove(s.Length - 1);
+        }
+
+        return s;
     }
 }
