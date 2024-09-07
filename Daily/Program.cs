9997651676749fedb7c29fa46420d62f25f24ce9
easyman121 <2854470@gmail.com>
2024-09-07 21:58:07 +0300

    Добавьте файлы проекта.

---
+++ Daily/Program.cs
@@ -0,0 +1,147 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        
+    }
+    
+    public int GetLucky(string s, int k)
+    {
+        string strnew = "";
+        foreach(char c in s)
+        {
+            strnew += (c - 96);
+
+        }
+        int result = 0;
+        for (int i =0; i < k; i++)
+        {
+            result = 0;
+            foreach (char c in strnew)
+            {
+                result += (c - '0');
+            }
+            strnew = "";
+            strnew += result;
+        }
+        return result;
+    }
+
+    public  int RobotSim(int[] commands, int[][] obstacles)
+    {
+        int[] position = new int[] { 0, 0 };
+        byte facing = 0;
+        HashSet<(int, int)> obstacleSet = new HashSet<(int, int)>();
+        
+        int furthestPoint = 0;
+        foreach (var obstacle in obstacles)
+        {
+            obstacleSet.Add((obstacle[0], obstacle[1]));
+        }
+        for (int i = 0; i < commands.Length; i++)
+        {
+
+            switch (commands[i])
+            {
+                case -2:
+                    facing = (byte)((facing + 1) % 4);
+                    break;
+                case -1:
+                    facing = (byte) ((facing + 3) % 4);
+                    break;
+                case int j when j > 0 && j < 10:
+
+                    for (j = 0; j < commands[i]; j++)
+                    {
+                        int nextX = position[0];
+                        int nextY = position[1];
+                        switch (facing)
+                        {
+                            case 0:
+                                nextY++;
+                                break;
+                            case 1:
+                                nextX--;
+                                break;
+                            case 3:
+                                nextX++;
+                                break;
+                            case 2:
+                                nextY--;
+                                break;
+                        }
+                        if (obstacleSet.Contains((nextX, nextY)))
+                        {
+                            break;
+                        }
+                        position[0] = nextX;
+                        position[1] = nextY;
+
+                    }
+                    break;
+            }
+            int newPoint = position[0] * position[0] + position[1] * position[1];
+            if (newPoint > furthestPoint)
+            {
+                furthestPoint = newPoint;
+            }
+        }
+        return furthestPoint;
+    }
+
+    public int[] MissingRolls(int[] rolls, int mean, int n)
+    {
+        int sum = 0;
+        foreach ( int roll in rolls ) 
+        {
+            sum += roll;
+        }
+        int remainingSum = mean * (n + rolls.Length) - sum;
+        if (remainingSum > 6 * n || remainingSum < n) return new int[] { };
+        int distributeMean = remainingSum/ n;
+        int mod = remainingSum % n;
+        int[] elements = new int[n];
+        Array.Fill(elements, distributeMean);
+        for (int i = 0; i < mod; i++)
+        {
+            elements[i]++;
+        }
+        return elements;
+    }
+
+    public class ListNode
+    {
+      public int val;
+     public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        HashSet<int> numSet = new HashSet<int>(nums);
+        ListNode dummy = new ListNode(0, head); 
+        ListNode current = dummy;
+
+        while (current.next != null)
+        {
+            if (numSet.Contains(current.next.val))
+            {
+                
+                current.next = current.next.next;
+            }
+            else
+            {
+               
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+
+
+    }
+
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,143 @@
+﻿
+public class Solution
+{
+    public static void Main()
+    {
+        
+        Console.ReadLine();
+    }
+    public string MergeAlternately(string word1, string word2)
+    {
+        string result = "";
+        int i = 0;
+        int j = 0;
+        while (i < word1.Length && j < word2.Length)
+        {
+            result += word1[i].ToString() + word2[j].ToString();
+            i++;
+            j++;
+        }
+        
+        if (j < word2.Length)
+        {
+            result += word2.Substring(j, word2.Length - j);
+        }
+        else if (i < word1.Length)
+        {
+            result += word1.Substring(i, word1.Length - i);
+            
+        }
+        return result;
+    }
+
+    public string GcdOfStrings(string str1, string str2)
+    {
+        string prefix = "";
+        string result ="";
+        int str1l = str1.Length;
+        int str2l = str2.Length;
+        for (int i = 0; i < str1l && i < str2l; i++)
+        {
+            prefix += str1[i].ToString();
+              
+            int prefl = prefix.Length;
+            if (str1l % prefl == 0 && str2l % prefl == 0)
+            {
+                if (str1 == string.Concat(Enumerable.Repeat(prefix, str1l/ prefl))
+                    && str2 == string.Concat(Enumerable.Repeat(prefix, str2l / prefl)))
+                {
+                    result = prefix;
+                }
+            }
+        }
+
+        return result;
+        
+    }
+
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        
+        int len = candies.Length;
+        IList<bool> result = new List<bool>(len);
+        int maxCandies = candies.Max();
+        for (int i = 0; i < len; i++)
+        {
+            if (candies[i] + extraCandies >= maxCandies)
+            {
+                result.Add(true);
+            }
+            else
+            {
+                result.Add(false);
+            }
+        }
+
+        return result;
+        /*
+         int len = candies.Length;
+        IList<bool> result = new List<bool>(len);
+for (int i = 0; i < len; i++)
+{
+    result.Add(false);
+}
+int maxCandies = candies.Max();
+for (int i = 0; i < len; i++)
+{
+    if (candies[i] + extraCandies >= maxCandies)
+    {
+        result[i] = true;
+    }
+}
+
+return result;
+         */
+    }
+
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 0 && n != 0)
+            {
+                bool check1 = true, check2 = true;
+                if (i - 1 >= 0)
+                {
+                    if (flowerbed[i - 1] != 0)
+                    {
+                        check1 = false;
+                    }
+                }
+
+                if (i + 1 <= flowerbed.Length)
+                {
+                    if (flowerbed[i + 1] != 0)
+                    {
+                        check2 = false;
+                    }
+                }
+
+
+                if (check1 && check2)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+                
+            }
+            
+        }
+        return n == 0 ? true : false;
+    }
+
+    public int ScoreOfString(string s)
+    {
+        int result = 0;
+        for (int i = 0; i < s.Length-1; i++)
+        {
+            int temp = (int)s[i] - (int)s[i + 1];
+            result += (temp + (temp >> 31)) ^ (temp >> 31);
+        }
+        return result;
+    }
+}
