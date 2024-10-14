7db6580fe3d5909aafa355360cc6ae48aa1023f0
easyman121 <2854470@gmail.com>
2024-10-14 13:06:42 +0300

    new

--- Daily/Program.cs
+++ Daily/Program.cs
@@ -1002,4 +1002,22 @@
         }
         return max;
     }
+
+    public long MaxKelements(int[] nums, int k) {
+
+        PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>();
+        foreach(int num in nums)
+        {
+            maxHeap.Enqueue(num , -num);
+        }
+        long score = 0 ;
+        for(int i = 0 ; i < k ; i++)
+        {
+            int largest = maxHeap.Dequeue();
+            score += largest;
+            int reduced = (int)Math.Ceiling(largest / 3D);
+            maxHeap.Enqueue(reduced , -reduced);
+        }
+        return score;
+    }
 }
