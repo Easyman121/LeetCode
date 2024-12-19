be36439af17dad8c23f01dd8f95075e68f692aa7
Easyman121 <2854470@gmail.com>
2024-12-19 12:08:38 +0300

    goida

--- Daily/Program.cs
+++ Daily/Program.cs
@@ -2039,4 +2039,14 @@ public class Solution
         }   
         return prices;
     }
+
+    public int MaxChunksToSorted(int[] arr) {
+        int topValue=0;
+        int totalChunks=0;
+        for(int i=0;i<arr.Length-1;i++){
+            topValue=Math.Max(topValue,arr[i]);
+            if(topValue == i) totalChunks++;
+        }
+        return totalChunks+1;
+    }
 }
